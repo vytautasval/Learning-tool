b44e1b8eee553099b51faaa19003d2e863d81a1c
Vytautas Valiulis <vytautasval40@gmail.com>
2023-08-13 15:10:18 +0300

    question mode work

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import time
+import sys
+import csv
+
 
 def main():
-    start_up()
+    if start_up() == "add":
+        questions_mode()
 
-#Initializes the start up and makes it possible to pick a mode.
+
+# Initializes the start up and makes it possible to pick a mode.
 def start_up():
     print(
         "Hello! This is an interactive learning tool. To begin, please type in one of the following:"
@@ -14,21 +19,61 @@ def start_up():
         "Type 'stats' to view question statistics.": "stats",
         "Type 'activate' to disable or enable questions.": "activate",
         "Type 'practice' to enter practice mode.": "practice",
-        "Type 'test' to enter test mode.": "test"
+        "Type 'test' to enter test mode.": "test",
     }
 
     for prompt, comm in command_prompts.items():
         print(prompt)
 
-    user_choice = input()
-    user_choice.casefold().strip()
+    user_choice = input().casefold().strip()
 
     while user_choice not in command_prompts.values():
-        print("Incorrect command. Please try again.")
+        print(
+            "Incorrect command. Please try again. If you wish to exit, type in 'quit'."
+        )
         user_choice = input()
+        if "quit" in user_choice:
+            raise (SystemExit("Program stopped."))
     else:
         print("Command accepted.")
         return user_choice
+
+
+# Enters question mode, provides the choice between quiz and free form mode and goes into the respective mode.
+def questions_mode():
+    print(
+        "You have chosen questions mode. You will now be able to enter and save your questions."
+    )
+    print(
+        "Please note, that a minimum of 5 questions are required. Enter 'done' once you're finished."
+    )
+
+    type = ""
+    while type != "done":
+        type = (
+            input("Enter 'quiz' or 'free form' to select question type: ")
+            .casefold()
+            .strip()
+        )
+        if "quiz" in type:
+            ...
+        elif "free form" in type:
+            free_form()
+
+#Inputs the question and answer to csv in free form.
+def free_form():
+    with open("questions.csv", "a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["question", "answer"])
+
+        if file.tell() == 0:
+            writer.writeheader()
+
+        question = input("Enter your question: ")
+        answer = input("Enter the answer: ")
+        writer.writerow({"question": question, "answer": answer})
+
+def quiz():
+
 
 
 if __name__ == "__main__":
