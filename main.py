00a427adc8c0382fd42826dd98a271443b82ff4a
Vytautas Valiulis <vytautasval40@gmail.com>
2023-08-14 16:06:58 +0300

    more oop conversion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import csv
 
+#Base tool itself.
 class LearningTool:
     def __init__(self):
         self.command_prompts = {
@@ -33,15 +34,26 @@ class LearningTool:
             print("Command accepted.")
             return user_choice
 
+    #Initializes the selected mode.
     def main(self):
         if self.start_up() == "add":
-            questions_mode()
+            question_mode = QuestionsMode()
+            question_mode.questions_mode()
+        if self.start_up() == "stats":
+            stats_mode = StatisticsMode()
+
 
 class QuestionsMode:
     def __init__(self):
         self.question_count = 0
+        self.id_counter = 1
 
-    # Enters question mode, provides the choice between quiz and free form mode and goes into the respective mode.
+    def get_next_question_id(self):
+        question_id = self.id_counter
+        self.id_counter += 1
+        return question_id
+
+    #Enters question mode, provides the choice between quiz and free form mode and goes into the respective mode.
     def questions_mode(self):
         print(
             "You have chosen questions mode. You will now be able to enter and save your questions."
@@ -49,20 +61,19 @@ class QuestionsMode:
         print(
             "Please note, that a minimum of 5 questions are required. Enter 'done' once you're finished."
         )
-
         with open("questions.csv") as file:
             reader = csv.DictReader(file)
             while True:
-                type = (
-                    input("Enter 'quiz' or 'free form' to select question type: ")
-                    .casefold()
-                    .strip()
-                )
-                if "quiz" in type:
-                    quiz()
-                elif "free form" in type:
-                    free_form()
-                if type == "done":
+                question_type = input("Enter 'quiz' or 'free form' to select question type: ").casefold().strip()
+
+                if question_type == "quiz":
+                    quiz_question = QuizQuestion()
+                    quiz_question.enter_question(self.get_next_question_id())
+                elif question_type == "free form":
+                    free_form_question = FreeFormQuestion()
+                    free_form_question.enter_question(self.get_next_question_id())
+
+                if question_type == "done":
                     for row in reader:
                         self.question_count += 1
                     if self.question_count < 5:
@@ -72,23 +83,27 @@ class QuestionsMode:
                         print("All questions have been added.")
                         break
 
+#Initializes the question and opens the csv file.
+class BaseQuestion:
+    def __init__(self):
+        self.file_path = "questions.csv"
 
-#Inputs the question and answer to csv in free form.
-def free_form():
-    with open("questions.csv", "a", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=["question", "answer"])
-
+    def enter_question(self, question_id):
         question = input("Enter your question: ")
-        answer = input("Enter the answer: ")
-        writer.writerow({"question": question, "answer": answer})
+        answer = self.get_answer()
 
-#Inputs the question and answer to csv in quiz form.
-def quiz():
-    with open("questions.csv", "a", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=["question", "answer"])
+        with open("questions.csv", "a", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "question", "answer"])
+            writer.writerow({"id": question_id, "question": question, "answer": answer})
 
-        question = input("Enter your question: ")
-        answer = ""
+#Returns the answer made in free form format.
+class FreeFormQuestion(BaseQuestion):
+    def get_answer(self):
+        return input("Enter the answer: ")
+
+#Returns the answer made in quiz format.
+class QuizQuestion(BaseQuestion):
+    def get_answer(self):
         answer_list = []
         while True:
             answer = input("Enter an answer:")
@@ -105,8 +120,12 @@ def quiz():
                 print("You've reached the maximum limit of 4 answers.")
                 break
 
-        writer.writerow({"question": question, "answer": answer_list})
+        return answer_list
 
+class StatisticsMode:
+    ...
 
 if __name__ == "__main__":
-    main()
+    learning_tool = LearningTool()
+    learning_tool.main()
+
